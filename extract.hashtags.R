822bf87a230971f981a95c0ccad60d708949e2df
Seraya Maouche <seraya.maouche@iscb.org>
2017-03-04 13:37:13 +0100

    Create extract.hashtags.R

---
+++ extract.hashtags.R
@@ -0,0 +1,14 @@
+extract.hashtags = function(vectorTweets){
+  
+  hash.pattern = "#[[:alpha:]]+"
+  have.hash = grep(x = vec, pattern = hash.pattern)
+  
+  hash.matches = gregexpr(pattern = hash.pattern,
+                          text = vec[have.hash])
+  extracted.hash = regmatches(x = vec[have.hash], m = hash.matches)
+  
+  df <- data.frame(table(tolower(unlist(extracted.hash))))
+  colnames(df) = c("tag","frequency")
+  df = df[order(df$frequency,decreasing = TRUE),]
+  return(df)
+}

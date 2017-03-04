75264a237f6f86095e0e3d6a108fedc4a2899eff
Seraya Maouche <seraya.maouche@iscb.org>
2017-03-04 13:59:17 +0100

    Update extract.hashtags.R

--- extract.hashtags.R
+++ extract.hashtags.R
@@ -1,13 +1,10 @@
 extract.hashtags = function(vectorTweets)
  {
-  
   hash.pattern = "#[[:alpha:]]+"
   have.hash = grep(x = vectorTweets, pattern = hash.pattern)
-  
   hash.matches = gregexpr(pattern = hash.pattern,
                           text = vectorTweets[have.hash])
   extract.hashtags = regmatches(x = vectorTweets[have.hash], m = hash.matches)
-  
   df = data.frame(table(tolower(unlist(extract.hashtags))))
   colnames(df) = c("tag","frequency")
   df = df[order(df$frequency,decreasing = TRUE),]
